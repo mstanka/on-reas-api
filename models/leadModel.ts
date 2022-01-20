dcb142ee975b242a2132c5f67555b368f52353e1
mstanka <stmmarketa@gmail.com>
2022-01-20 19:05:58 +0100

    add lead schema

---
+++ models/leadModel.ts
@@ -0,0 +1,64 @@
+import mongoose from 'mongoose';
+import isEmail from 'validator/lib/isEmail';
+import isMobilePhone from 'validator/lib/isMobilePhone';
+
+const leadSchema = new mongoose.Schema({
+	estateType: {
+		type: String,
+		required: [true, 'A lead must have an estate type.'],
+		trim: true,
+	},
+	fullName: {
+		type: String,
+		requiredL: [true, 'A lead must have a name.'],
+		trim: true,
+	},
+	// @ts-ignore
+	phone: {
+		type: String,
+		required: [true, 'A lead must have a phone.'],
+		trim: true,
+		validate: [isMobilePhone],
+	},
+	// @ts-ignore
+	email: {
+		type: String,
+		required: [true, 'A lead must have a phone'],
+		trim: true,
+		validate: [isEmail],
+	},
+	region: {
+		type: String,
+		required: [true, 'A lead must have a region.'],
+		enum: {
+			values: [
+				'Hlavní město Praha',
+				'Středočeský kraj',
+				'Jihočeský kraj',
+				'Plzeňský kraj',
+				'Karlovarský kraj',
+				'Ústecký kraj',
+				'Liberecký kraj',
+				'Královéhradecký kraj',
+				'Pardubický kraj',
+				'Kraj Vysočina',
+				'Jihomoravský kraj',
+				'Zlínský kraj',
+				'Olomoucký kraj',
+				'Moravskoslezský kraj',
+			],
+			message: [
+				`Region must be 'Hlavní město Praha', 'Středočeský kraj', 'Jihočeský kraj', 'Plzeňský kraj', 'Karlovarský kraj','Ústecký kraj', 'Liberecký kraj', 'Královéhradecký kraj', 'Pardubický kraj', 'Kraj Vysočina', 'Jihomoravský kraj','Zlínský kraj', 'Olomoucký kraj' or 'Moravskoslezský kraj'`,
+			],
+		},
+	},
+	district: {
+		type: String,
+		required: [true, 'A lead must have a district'],
+		trim: true,
+	},
+});
+
+const Lead = mongoose.model('Lead', leadSchema);
+
+export default Lead;
