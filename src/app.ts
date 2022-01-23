a8ee438f9651d7618b611ed913af44b14df77f37
mstanka <stmmarketa@gmail.com>
2022-01-23 18:59:42 +0100

    add cors

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,23 @@
 import express from 'express';
+import cors from 'cors';
 import leadRouter from './routes/leadRoute';
+
+interface ICorsOptions {
+    origin: string;
+    optionsSuccessStatus: number;
+}
+
+const corsOptions: ICorsOptions = {
+	origin: 'https://on-reas-web.vercel.app',
+	optionsSuccessStatus: 200,
+}
 
 const app = express();
 
 app.use(express.json());
+app.use(cors(corsOptions));
 
-app.use('/', leadRouter)
+
+app.use('/', leadRouter);
 
 export default app;
